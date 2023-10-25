bbcd79badc78cc55ced0533517a333326f173597
raine-detmer <74794882+raine-detmer@users.noreply.github.com>
2023-10-25 16:06:20 -0700

    add distinction of whether delta AIC was >=2

--- functions/lin_check.R
+++ functions/lin_check.R
@@ -17,7 +17,7 @@
 
 
 lin_check <- function(simdt, thresh_methods = c("abs_max_d2", "min_d2", "zero_d2", "zero_d1"),
-                      sig_criteria = c("none", "jack_quant", "sim_int"), knots = 4, smooth_type = "tp", span = 0.1){
+                      sig_criteria = c("none", "jack_quant", "sim_int"), z_scale = TRUE, knots = 4, smooth_type = "tp", span = 0.1){
 
   # get the number of simulations that were run
   nsim <- length(unique(simdt$sim))
@@ -25,13 +25,31 @@ lin_check <- function(simdt, thresh_methods = c("abs_max_d2", "min_d2", "zero_d2
   # holding vector for which model is best (lowest AIC)
   best_mod <- rep(NA, nsim)
 
+  # holding vector for differences in AIC
+  aic_diff <- rep(NA, nsim)
+
   for(i in 1:nsim){ # for each simulation replicate
 
     # get the data
     datIN <- simdt[which(simdt$sim == i), ]
 
+    if(z_scale==TRUE){
+      datIN$driver <- zfun(datIN$driver)
+      datIN$obs_response <- zfun(datIN$obs_response)
+    }
+
     # fit the gam
-    gam_full <- gam(obs_response~s(driver,k=knots,bs=smooth_type),data = datIN) # fit a gam to the jackd data set
+    #if(z_scale==TRUE){
+    #gam_full <- gam(obs_response~ -1 + s(driver,k=knots,bs=smooth_type),data = datIN) # fit a gam to the data set
+    #lm_full <- lm(obs_response ~ 0 + driver, data = datIN)
+
+    #} else{
+     # gam_full <- gam(obs_response~s(driver,k=knots,bs=smooth_type),data = datIN) # fit a gam to the jackd data set
+     # lm_full <- lm(obs_response ~ driver, data = datIN)
+   # }
+
+
+    gam_full <- gam(obs_response~s(driver,k=knots,bs=smooth_type),data = datIN) # fit a gam to the data set
 
     # fit the linear model
     lm_full <- lm(obs_response ~ driver, data = datIN)
@@ -41,11 +59,24 @@ lin_check <- function(simdt, thresh_methods = c("abs_max_d2", "min_d2", "zero_d2
     lm_aic <- AIC(lm_full)
 
     if(gam_aic < lm_aic){
-      best_mod[i] <- "gam"
+
+      if(abs(lm_aic - gam_aic) >=2){
+        best_mod[i] <- "gam"
+      } else{
+        best_mod[i] <- "gam_ns"
+      }
+
     } else {
-      best_mod[i] <- "lm"
+
+      if(abs(lm_aic - gam_aic) >=2){
+        best_mod[i] <- "lm"
+      } else{
+        best_mod[i] <- "lm_ns"
+      }
+
     }
 
+    aic_diff[i] <- lm_aic - gam_aic # values > +2 mean gam is better, values < -2 mean lm is better
 
   }
 
@@ -64,6 +95,7 @@ lin_check <- function(simdt, thresh_methods = c("abs_max_d2", "min_d2", "zero_d2
   df <- data.frame(
     sim = rep(nsim_ss, length(thresh_methods)),
     best_mod = rep(best_mod, length(thresh_methods)*length(sig_criteria)),
+    aic_diff = rep(aic_diff, length(thresh_methods)*length(sig_criteria)),
     thresh_method = rep(thresh_methods_ss, each = nsim),
     sig_type = rep(sig_criteria, nsim*length(thresh_methods))
   )
